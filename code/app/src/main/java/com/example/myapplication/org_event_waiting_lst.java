a77a3ced9c8314463c4cb6f3ad4dcff28a3b8b0e
peterhuang569 <peterzdhuang@gmail.com>
2024-10-19 21:54:35 -0600

    finished the skeleton of the organizer navgraph

--- code/app/src/main/java/com/example/myapplication/org_event_waiting_lst.java
+++ code/app/src/main/java/com/example/myapplication/org_event_waiting_lst.java
@@ -12,10 +12,10 @@ import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link org_events_lst#newInstance} factory method to
+ * Use the {@link org_event_waiting_lst#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class org_events_lst extends Fragment {
+public class org_event_waiting_lst extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class org_events_lst extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public org_events_lst() {
+    public org_event_waiting_lst() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class org_events_lst extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment org_events_lst.
+     * @return A new instance of fragment org_event_waiting_lst.
      */
     // TODO: Rename and change types and number of parameters
-    public static org_events_lst newInstance(String param1, String param2) {
-        org_events_lst fragment = new org_events_lst();
+    public static org_event_waiting_lst newInstance(String param1, String param2) {
+        org_event_waiting_lst fragment = new org_event_waiting_lst();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -60,28 +60,25 @@ public class org_events_lst extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_org_events_lst, container, false);
+        View view = inflater.inflate(R.layout.fragment_org_event_waiting_lst, container, false);
 
-        Button button_go_to_home_from_org_event_list = view.findViewById(R.id.button_go_to_home_from_org_event_list);
-        button_go_to_home_from_org_event_list.setOnClickListener(v ->
-                Navigation.findNavController(v).navigate(R.id.action_org_events_lst_to_home)
-        );
-
-// Button to navigate to Event Details
-        Button button_go_to_event_from_org_event_list = view.findViewById(R.id.button_go_to_event_from_org_event_list);
-        button_go_to_event_from_org_event_list.setOnClickListener(v ->
-                Navigation.findNavController(v).navigate(R.id.action_org_events_lst_to_org_event)
-        );
-        Button buttonGoToAddEvent = view.findViewById(R.id.button_go_to_add_event_from_org_events_lst);
-        buttonGoToAddEvent.setOnClickListener(v ->
-                Navigation.findNavController(v).navigate(R.id.action_org_events_lst_to_org_add_event)
-        );
-        Button buttonGoToEditOrganizer = view.findViewById(R.id.button_go_to_edit_organizer_from_org_events_lst);
-        buttonGoToEditOrganizer.setOnClickListener(v ->
-                Navigation.findNavController(v).navigate(R.id.action_org_events_lst_to_org_edit_organizer)
+        // Button to navigate to the Selected List
+        Button buttonGoToSelectedList = view.findViewById(R.id.button_go_to_selected_list_from_org_event_waiting_lst);
+        buttonGoToSelectedList.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_org_event_waiting_lst_to_org_event_selected_lst)
         );
 
+        // Button to navigate to Notifications
+        Button buttonGoToNotif = view.findViewById(R.id.button_go_to_notif_from_org_event_waiting_lst);
+        buttonGoToNotif.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_org_event_waiting_lst_to_org_notif_waiting_lst)
+        );
+
+        // Button to navigate to Map
+        Button buttonGoToMap = view.findViewById(R.id.button_go_to_map_from_org_event_waiting_lst);
+        buttonGoToMap.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_org_event_waiting_lst_to_org_map)
+        );
 
         return view;
     }
