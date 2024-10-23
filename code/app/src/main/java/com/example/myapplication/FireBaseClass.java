3fac87e154ba5732045cb8cc2b0f6f68e2618669
Cyrus Garg <cyrus1@ualberta.ca>
2024-10-23 16:02:07 -0600

    saving for pull

---
+++ code/app/src/main/java/com/example/myapplication/FireBaseClass.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+abstract public class FireBaseClass {
+    private String FireBaseId;
+
+    public FireBaseClass() {
+    }
+
+    public String getFireBaseId() {
+        return FireBaseId;
+    }
+
+    public void setFireBaseId(String FireBaseId) {
+        this.FireBaseId = FireBaseId;
+    }
+}
---
+++ code/app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+public class User extends FireBaseClass{
+    private String name;
+    private Integer deviceID;
+    private String email;
+    private Integer phoneNumber;
+
+}
