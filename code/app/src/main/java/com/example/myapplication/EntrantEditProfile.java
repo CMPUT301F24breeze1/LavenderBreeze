e0605f6b0193e318d09940bb0dd1a7c6b6948bbe
Cyrus Garg <cyrus1@ualberta.ca>
2024-10-29 23:16:43 -0600

    saving the changes

--- code/app/src/main/java/com/example/myapplication/EntrantEditProfile.java
+++ code/app/src/main/java/com/example/myapplication/EntrantEditProfile.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -9,6 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +21,11 @@ import android.widget.Button;
  * create an instance of this fragment.
  */
 public class EntrantEditProfile extends Fragment {
+    private Button doneEditButton;
+    private ImageButton editPhotoButton, editNameButton, editEmailButton, editPhoneButton;
+    private ImageView profilePicture;
+    private TextView personName, emailAddress, contactPhoneNumber;
+    private SwitchCompat emailNotificationSwitch;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,11 +73,27 @@ public class EntrantEditProfile extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_entrant_edit_profile, container, false);
 
-        // Find the button and set an onClickListener to navigate to org_event_lst.xml
-        Button profile = view.findViewById(R.id.button_go_to_entrant_profile);
-        profile.setOnClickListener(v ->
+        // Initialize views
+        doneEditButton = view.findViewById(R.id.doneEdit);
+        editPhotoButton = view.findViewById(R.id.editPhotoButton);
+        editNameButton = view.findViewById(R.id.editNameButton);
+        editEmailButton = view.findViewById(R.id.editEmailButton);
+        editPhoneButton = view.findViewById(R.id.editPhoneButton);
+        profilePicture = view.findViewById(R.id.profilePicture);
+        personName = view.findViewById(R.id.personName);
+        emailAddress = view.findViewById(R.id.emailAddress);
+        contactPhoneNumber = view.findViewById(R.id.contactPhoneNumber);
+        emailNotificationSwitch = view.findViewById(R.id.emailNotificationSwitch);
+
+        doneEditButton.setOnClickListener(v ->
                 Navigation.findNavController(v).navigate(R.id.action_entrantEditProfile_to_entrantProfile3)
         );
+
+        // Find the button and set an onClickListener to navigate to org_event_lst.xml
+//        Button profile = view.findViewById(R.id.button_go_to_entrant_profile);
+//        profile.setOnClickListener(v ->
+//                Navigation.findNavController(v).navigate(R.id.action_entrantEditProfile_to_entrantProfile3)
+//        );
         return view;
     }
 }
