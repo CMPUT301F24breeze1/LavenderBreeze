0ecd815d86d7d5cbda57c369dd2c82d6beb4b163
peterhuang569 <peterzdhuang@gmail.com>
2024-12-01 16:29:41 -0700

    bug

--- code/app/src/androidTest/java/com/example/myapplication/AdminPicturesListTest.java
+++ code/app/src/androidTest/java/com/example/myapplication/AdminPicturesListTest.java
@@ -11,6 +11,7 @@ import android.util.Log;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.GrantPermissionRule;
 import androidx.test.runner.permission.UiAutomationPermissionGranter;
 
 import com.example.myapplication.model.Facility;
@@ -32,14 +33,11 @@ public class AdminPicturesListTest {
     @Rule
     public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
 
-//    @Rule
-//    public UiAutomationPermissionGranter permissionGranter = new UiAutomationPermissionGranter();
-//
-//    @Rule
-//    public void setPermissionGranter(UiAutomationPermissionGranter permissionGranter) {
-//        this.permissionGranter = permissionGranter;
-//        permissionGranter.addPermissions("Manifest.permissions.ACCESS_FINE_LOCATION","Manifest.permissions.CAMERA");
-//    }
+    @Rule
+    public GrantPermissionRule permissionLocation = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+    @Rule
+    public GrantPermissionRule permissionRead = GrantPermissionRule.grant(android.Manifest.permission.POST_NOTIFICATIONS);
+
 
     @Before
     public void CreateUser() throws InterruptedException {
